a8ac6153721ed1d25c67a38ba685c3a4df3cf5b2
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-02-08 18:20:40 -0300

    a

--- Python_Word.py
+++ Python_Word.py
@@ -1,1 +1,15 @@
-import docx
+from docx import Document
+
+documento = Document()  # Criando um documento
+
+# Abrindo arquivo já existente
+arquivo = open('arquivo_existente.docx', 'rb')
+
+documento = Document(arquivo)
+
+arquivo.close()
+
+documento = Document('arquivo_existente.docx')
+
+# Salvando arquivo novo
+documento.save('arquivo_salvo.docx')
